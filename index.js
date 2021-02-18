99a5cbb8062695aaeb06ae1724ed6085d0170b18
Clayton Lewis <lewisclayton89@gmail.com>
2021-02-18 04:30:24 -0500

    modifications

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 let videoBtn = document.querySelector(".video-btn");
 let words = document.querySelector(".words");
-let bio = document.querySelector(".bio")
+let bio = document.querySelector(".bio");
 let videos = document.querySelector(".videos");
-let menu = document.querySelector(".menu")
+let menu = document.querySelector(".menu");
 
-menu.onclick = (ev) => { 
-  let target = ev.target.className
-  if(target === 'videos') {
-    window.location.href = "./videos.html"
-  } else if(target === 'bio') {
-    window.location.href = './about.html'
+menu.onclick = (ev) => {
+  let target = ev.target.className;
+  if (target === "videos") {
+    window.location.href = "./videos.html";
+  } else if (target === "bio") {
+    window.location.href = "./about.html";
   }
-  
 };
 
-window.onload = () => { words.style.opacity = "1" };
+words.innerHTML = "THANK YOU FOR LOOKING AT MY WEB PAGE!";
 
-setTimeout(() => { words.style.opacity = "0" }, 4000);
+setTimeout(() => {
+  words.style.opacity = "1";
+ 
+}, 1000);
 
-//setTimeout(() => { words.style.display = "none"}, 8000);
 
-setTimeout(() => { 
-  bio.style.opacity = "1" 
-  videos.style.opacity = "1"
-}, 6000);
+setTimeout(() => {
+  words.style.opacity = "0";
+}, 4000);
+
+/*
+setTimeout(() => {
+  words.style.display = "none";
+}, 8000);
+*/
+
+setTimeout(() => {
+  bio.style.width = "100%";
+  videos.style.width = "100%";
+  bio.style.opacity = "1";
+  videos.style.opacity = "1";
+  bio.innerHTML = "BACKGROUND";
+  videos.innerHTML = "DEMO REELS";
+}, 7000);
 
 console.log("height is " + window.innerHeight);
 console.log("width is " + window.innerWidth);
