82961d25c780e7a05e7e93dac043485236585756
Clayton Lewis <lewisclayton89@gmail.com>
2021-03-21 19:01:36 -0400

    open in new tab

--- game-intro.js
+++ game-intro.js
@@ -1,11 +1,18 @@
-let realRap = document.querySelector('.real-rap');
+//let realRap = document.querySelector('.real-rap');
 let portfolio = document.querySelector('.portfolio')
+//let gitHub = document.querySelector('.github')
 
-realRap.onclick = () => {
-    window.location.href = 'https://c-lew-on-truth.github.io/real-rap-fans/'
-}
+document.querySelector('.real-rap').onclick = function() {
+    var redirectWindow = window.open('https://c-lew-on-truth.github.io/real-rap-fans/', '_blank');
+    redirectWindow.location;
+};
 
 portfolio.onclick = () => {
     //console.log('seen')
     window.location.href = 'index.html'
 }
+
+document.querySelector('.github').onclick = function() {
+    var redirectWindow = window.open('https://github.com/C-Lew-On-Truth', '_blank');
+    redirectWindow.location;
+};
